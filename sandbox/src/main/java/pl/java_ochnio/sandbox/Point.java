40754f22cd54e72682c87f6032eaa4b37487b27c
Justyna Ochnio <jochnio@sii.pl>
2022-06-01 18:52:20 +0200

    Adding new java classes

---
+++ sandbox/src/main/java/pl/java_ochnio/sandbox/Point.java
@@ -0,0 +1,10 @@
+package pl.java_ochnio.sandbox;
+
+public class Point {
+  public static void main(String args[]) {
+    double X = 10.80;
+    double Y = 15.65;
+    System.out.println("Współrzędne punktu to " + X + " . " + Y);
+  }
+
+}
