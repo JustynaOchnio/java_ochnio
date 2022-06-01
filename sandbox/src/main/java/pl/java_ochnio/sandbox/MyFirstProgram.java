a5df4148cba70fd51cd4b54100baa859d76deab5
Justyna Ochnio <jochnio@sii.pl>
2022-06-01 17:56:01 +0200

    Dodanie kilku funkcji

--- sandbox/src/main/java/pl/java_ochnio/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/java_ochnio/sandbox/MyFirstProgram.java
@@ -3,6 +3,27 @@ package pl.java_ochnio.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+    hello("Justyna");
+
+    double l = 5;
+    System.out.println("Powierzchnia kwadratu o boku " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Powierzchnia prostokąta o bokach " + a + " i " + b +" = " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area (double len) {
+    return len * len;
+  }
+
+  public static double area (double a, double b) {
+    return a * b;
   }
 }
