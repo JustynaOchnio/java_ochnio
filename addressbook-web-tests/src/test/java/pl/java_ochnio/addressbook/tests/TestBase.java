17950bc4d2bcb6d931656b5f9115d338331b1c1a
JustynaOchnio <jochnio@sii.pl>
2022-06-18 14:57:27 +0200

    Adding new browsers

--- addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package pl.java_ochnio.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pl.java_ochnio.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
