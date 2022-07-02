a3fb0ed292355283588290e9fd4f7c4bebc9a816
JustynaOchnio <jochnio@sii.pl>
2022-07-02 20:27:00 +0200

    Conditions - task no 9

--- addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/appmanager/ContactHelper.java
@@ -37,7 +37,7 @@ public class ContactHelper extends HelperBase {
     }
 
     public void initContactModification() {
-        click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td/a/img"));
+        click(By.xpath("//img[@alt='Edit']"));
     }
 
     public void submitContactModification() {
--- addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/ContactModificationTests.java
@@ -8,13 +8,13 @@ public class ContactModificationTests extends TestBase {
     @Test
     public void testContactModification() {
         app.getNavigationHelper().goToHomePage();
-       // if (! app.getContactHelper().isThereAContact()) {
-           // app.getContactHelper().createContact(new ContactData
-                    //("test1", null, null, null, null, null, "test1"), true);
-      //  }
+        if (!app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().createContact(new ContactData
+                    ("test1", null, null, null, null, null, "test1"), true);
+        }
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new ContactData
-                ("test1", null, null, null, null, null, "test1"), false);
+                ("test1", "test2", "test3", "test4", "4325435", "test@test", "test1"), false);
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToContactPage();
     }
