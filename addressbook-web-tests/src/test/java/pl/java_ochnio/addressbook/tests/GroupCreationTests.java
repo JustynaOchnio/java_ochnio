365f1a9f79f64d71903e1112c1a7a677a6fd9a48
JustynaOchnio <jochnio@sii.pl>
2022-07-06 12:03:22 +0200

    Sorting in Group Creation/Modification Tests

--- addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/GroupCreationTests.java
@@ -3,8 +3,8 @@ package pl.java_ochnio.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import pl.java_ochnio.addressbook.model.GroupData;
-import java.util.HashSet;
-import java.util.List;
+
+import java.util.*;
 
 public class GroupCreationTests extends TestBase {
 
@@ -17,9 +17,10 @@ public class GroupCreationTests extends TestBase {
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
-
 }
