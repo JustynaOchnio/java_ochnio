260b5b3f2fbd9b9d9daf8d3c96e15ff31753a2dc
JustynaOchnio <jochnio@sii.pl>
2022-07-10 16:55:11 +0200

    Using comparator for Contacts

--- addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/ContactCreationTests.java
@@ -2,9 +2,7 @@ package pl.java_ochnio.addressbook.tests;
 
 import org.testng.Assert;
 import org.testng.annotations.*;
-
 import pl.java_ochnio.addressbook.model.ContactData;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,18 +11,12 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void testContactCreation() throws Exception {
         List<ContactData> before = app.getContactHelper().getContactList();
-        ContactData contact = new ContactData(before.get(before.size() - 1).getId(), "firstname", "lastname", "test3", "test4", "4325435", "test@test", "test1");
+        ContactData contact = new ContactData("firstname", "lastname", "test3", "test4", "4325435", "test@test", "test1");
         app.getContactHelper().createContact(contact, true);
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (ContactData c : after) {
-            if (c.getId() > max) {
-                max = c.getId();
-            }
-        }
-        contact.setId(max);
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(contact);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
