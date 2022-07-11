d97a3278f0f89368a3f1bac142201e7d54023b27
JustynaOchnio <jochnio@sii.pl>
2022-07-11 14:29:25 +0200

    Before/After Suite instead of Method

--- addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/TestBase.java
@@ -1,20 +1,20 @@
 package pl.java_ochnio.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import pl.java_ochnio.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeSuite
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterSuite
     public void tearDown() throws Exception {
         app.stop();
     }
