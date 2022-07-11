94c0ff4e1f150524b926506f0034de2803836247
JustynaOchnio <jochnio@sii.pl>
2022-07-11 18:59:48 +0200

    Builder for Groups

--- addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/GroupCreationTests.java
@@ -3,7 +3,6 @@ package pl.java_ochnio.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import pl.java_ochnio.addressbook.model.GroupData;
-
 import java.util.*;
 
 public class GroupCreationTests extends TestBase {
@@ -12,7 +11,7 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() throws Exception {
         app.goTo().groupPage();
         List<GroupData> before = app.group().list();
-        GroupData group= new GroupData("test2", null, null);
+        GroupData group= new GroupData().withName("test2");
         app.group().create(group);
         List<GroupData> after = app.group().list();
         Assert.assertEquals(after.size(), before.size() + 1);
