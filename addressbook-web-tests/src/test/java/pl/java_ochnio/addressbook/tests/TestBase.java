1e1038a2a1348bc7ba738e40548283cee163f605
JustynaOchnio <jochnio@sii.pl>
2022-07-25 11:11:22 +0200

    Test configuration, local properties

--- addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/TestBase.java
@@ -5,9 +5,20 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import pl.java_ochnio.addressbook.appmanager.ApplicationManager;
 
+import javax.sql.rowset.BaseRowSet;
+import java.io.IOException;
+
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app;
+
+    static {
+        try {
+            app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @BeforeSuite
     public void setUp() throws Exception {
