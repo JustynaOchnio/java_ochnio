f91f628704418bd7eaf12797aeb8ad91199ce8db
JustynaOchnio <jochnio@sii.pl>
2022-07-29 15:11:40 +0200

    Verifying lisst on UI and DB

--- addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/java_ochnio/addressbook/tests/TestBase.java
@@ -1,5 +1,6 @@
 package pl.java_ochnio.addressbook.tests;
 
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,10 +9,16 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import pl.java_ochnio.addressbook.appmanager.ApplicationManager;
+import pl.java_ochnio.addressbook.model.GroupData;
+import pl.java_ochnio.addressbook.model.Groups;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
@@ -44,5 +51,15 @@ public class TestBase {
     @AfterMethod(alwaysRun = true)
     public void logTestStop(Method m) {
         logger.info("Stop test " + m.getName());
+    }
+
+    public void verifyGroupListInUi() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.group().all();
+            assertThat(uiGroups, equalTo(dbGroups.stream()
+                    .map(g -> new GroupData().withId(g.getId()).withName(g.getName()))
+                    .collect(Collectors.toSet())));
+        }
     }
 }
