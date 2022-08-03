f0d8cec63618dc15afa33885b43b61bde0679ca7
JustynaOchnio <jochnio@sii.pl>
2022-08-03 12:43:44 +0200

    Adding new module for Mantis

---
+++ mantis-tests/src/test/java/pl/java_ochnio/mantis/TestBase.java
@@ -0,0 +1,31 @@
+package pl.java_ochnio.mantis;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import pl.java_ochnio.mantis.appmanager.ApplicationManager;
+import java.io.IOException;
+
+public class TestBase {
+
+    protected static final ApplicationManager app;
+
+    static {
+        try {
+            app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+
+}
