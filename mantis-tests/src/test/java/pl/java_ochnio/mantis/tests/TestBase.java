07159dccb13ad4e919fdf5aa97789094cae3eb29
JustynaOchnio <jochnio@sii.pl>
2022-08-08 11:36:15 +0200

    FTP Helper

--- mantis-tests/src/test/java/pl/java_ochnio/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/pl/java_ochnio/mantis/tests/TestBase.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import pl.java_ochnio.mantis.appmanager.ApplicationManager;
+
+import java.io.File;
 import java.io.IOException;
 
 public class TestBase {
@@ -21,10 +23,12 @@ public class TestBase {
     @BeforeSuite
     public void setUp() throws Exception {
         app.init();
+        app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.back");
     }
 
     @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
+        app.ftp().restore("config_inc.php.back", "config_inc.php");
         app.stop();
     }
 
